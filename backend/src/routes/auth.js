307e19b2eb1adb58bf812a37666cc75662b650ba
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-23 00:18:30 -0400

    Add POST /login route for user login

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -9,8 +9,34 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/login', async (req, res) => {
     try {
+        const { email, password } = req.body;
 
+        const result = await pool.query(
+            `SELECT * FROM users WHERE email = $1`,
+            [email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(400).json({
+                error: 'Error logging in to account!'
+            });
+        }
+
+        const user = result.rows[0]
+        const loginResult = await bcrypt.compare(password, user.password);
+
+        if (!loginResult) {
+            return res.status(400).json({
+                error: 'Email or password is invalid! Please try again.'
+            });
+        }
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({ token, user: { id: user.id, name: user.name, email: user.email } });
     } catch (error) {
+        console.error('Error with login!', error);
+        res.status(500).json({ error: 'Error! Could not login user.' });
 
     }
 });
