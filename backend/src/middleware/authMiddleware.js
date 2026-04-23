458b27231dab882399c412c21e48391affac0ca4
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-22 22:05:06 -0400

    Add auth middleware skeleton

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export default authMiddleware;
