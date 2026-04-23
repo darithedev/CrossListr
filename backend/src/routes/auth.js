c68236fef8c9afbeaa90b513fe7189ddc6918793
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-22 23:31:15 -0400

    Add POST /signup route for user account creation/signup

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -17,9 +17,25 @@ router.post('/login', async (req, res) => {
 
 router.post('/signup', async (req, res) => {
     try {
+        const { name, phone_number, email, password } = req.body;
 
+        const passwordBcrypt = await bcrypt.hash(password, 12);
+
+        const result = await pool.query(
+            `INSERT INTO users (name, phone_number, email, password)
+            VALUES ($1, $2, $3, $4)
+            RETURNING id, name, phone_number, email`,
+            [name, phone_number, email, passwordBcrypt]
+        );
+
+        const user = result.rows[0];
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({ token, user });
     } catch (error) {
-
+        console.error('Error with signup!', error);
+        res.status(500).json({ error: 'Error! Could not signup user.' });
     }
 });
 
