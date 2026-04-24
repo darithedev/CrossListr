ea705fc6ac1504a97b08eaca50d331fbd8d4260c
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-24 18:20:54 -0400

    Import useState (react), Form, Button (react-bootstrap), and useNavigate(react-router-dom), add SignupData typescript type alias with name, phone_number, email, and password, add navigate const for route navigations, add handleSubmit, refactor div to Form

--- frontend/src/routes/Signup.tsx
+++ frontend/src/routes/Signup.tsx
@@ -1,9 +1,36 @@
-const Signup = () => {
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
+const Signup = () => {
+    const navigate = useNavigate();
+    
+    type SignupData = {
+        name: string;
+        phone_number: string;
+        email: string;
+        password: string;
+    };
+
+    const [signupData, setSignupData] = useState<SignupData>({
+        name: "",
+        phone_number: "",
+        email: "",
+        password: ""
+    });
+
+    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        alert("Submitted!")
+        clearLogin();
+    }
     return (
-        <div className="signup-container">
-            <h1>Hello Signup Route</h1>
-        </div>
+        <Form 
+            className="singup-form"
+            onSubmit={handleSubmit}
+        >
+            
+        </Form>
     )
 }
 
