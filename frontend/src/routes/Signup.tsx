7fd41ca3f358e9eb0052a8d0d66372c564b7e8b4
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-24 18:22:50 -0400

    Add name, phone_number, email, and password handlers, add clearLogin function

--- frontend/src/routes/Signup.tsx
+++ frontend/src/routes/Signup.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
     const navigate = useNavigate();
-    
+
     type SignupData = {
         name: string;
         phone_number: string;
@@ -18,6 +18,35 @@ const Signup = () => {
         email: "",
         password: ""
     });
+
+    const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.value;
+        setSignupData((prev) => ({ ...prev, name}));
+    };
+
+    const handlePhoneNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const phone_number = event.target.value.replace(/\D/g, '');
+        setSignupData((prev) => ({ ...prev, phone_number}));
+    };
+
+    const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const email = event.target.value;
+        setSignupData((prev) => ({ ...prev, email}));
+    };
+
+        const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const password = event.target.value;
+        setSignupData((prev) => ({ ...prev, password}));
+    };
+
+    const clearLogin = () => {
+        setSignupData({
+            name: "",
+            phone_number: "",
+            email: "",
+            password: ""
+        });
+    };
 
     const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
