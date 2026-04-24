00d7901d5b991a2a1de863f6a31cdac6dc4e59e7
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-24 00:54:33 -0400

    Add Login form without submit hitting backend

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,9 +1,77 @@
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+
 const Login = () => {
+    const navigate = useNavigate();
+    type LoginData = {
+        name: string;
+        email: string;
+    };
+
+    const [loginData, setLoginData] = useState<LoginData>({
+        name: "",
+        email: ""
+    });
+
+    const handleName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.value;
+        setLoginData((prev) => ({ ...prev, name}));
+    };
+
+    const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const email = event.target.value;
+        setLoginData((prev) => ({ ...prev, email}));
+    };
+
+    const clearLogin = () => {
+        setLoginData({
+            name: "",
+            email: ""
+        });
+    }
+
+    const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        alert("Submitted!")
+        clearLogin();
+    }
 
     return (
-        <div className="home-container">
-            <h1>Hello Login Route</h1>
-        </div>
+        <Form 
+            className="login-form"
+            onSubmit={handleSubmit}
+        >
+            <h1>Login</h1>
+            <Form.Group controlId="name">
+                <Form.Label>Name: </Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Riley Cares"
+                    required
+                    value={loginData.name}
+                    onChange={handleName}
+                />
+            </Form.Group>
+            <Form.Group controlId="email">
+                <Form.Label>Email: </Form.Label>
+                <Form.Control
+                    type="email"
+                    value={loginData.email}
+                    placeholder='example@mail.com'
+                    onChange={handleEmail}
+                />
+            </Form.Group>
+            <Button type="submit" variant="outline-success">
+                Submit
+            </Button>
+            <Button type="button" variant="outline-warning" onClick={clearLogin}>
+                Reset
+            </Button>
+            {/*<Button type="button" variant="outline-warning" onClick={() => navigate("/home")}>
+                Back
+            </Button>*/}
+        </Form>
     )
 }
 
