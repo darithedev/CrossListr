4cf2470e291c20dcc143bd20010e5b950c9e73a1
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-28 01:16:37 -0400

    Refactor authMiddleware comma to period, and spelling for decoded

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
     if(!JWT_SECRET) {
-        return res.status(500),json({ error: "JWT secret is required. Please configure."})
+        return res.status(500).json({ error: "JWT secret is required. Please configure."})
     }
     try {
         const authHeader = req.headers.authorization;
@@ -18,7 +18,7 @@ const authMiddleware = (req, res, next) => {
         } else {
             const token = authHeader.slice(7);
             const decoded = jwt.verify(token, JWT_SECRET);
-            req.userId = decode.userId;
+            req.userId = decoded.userId;
         }
         next();
     } catch (error) {
