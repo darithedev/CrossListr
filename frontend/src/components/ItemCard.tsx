2d1dffb29e008087287ea5409fdcdfbcb8ea8f56
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-29 01:52:51 -0400

    Add item card logic that creates a display card with image, title, and button for each item

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -1,5 +1,29 @@
-const ItemCard = () => {
+import { useState } from 'react'
+import { Card, Button } from 'react-bootstrap'
 
+type Item = {
+    id: string;
+    title: string;
+    item_images: string[];
+}
+
+const ItemCard = () => {
+    const [items, setItems] = useState<Item[]>([]);
+
+    return (
+        <>
+            {items.map((item) => (
+                <Card key={item.id} style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={item.item_images[0]} />
+                     <Card.Body>
+                        <Card.Title>{item.title}</Card.Title>
+                        <Button variant="primary">Select</Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
+       
+    )
 }
 
 export default ItemCard;
