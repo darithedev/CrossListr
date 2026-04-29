30e295002b4912fbb70ebc2f8f06519550011637
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-29 01:02:10 -0400

    Import Home route and ProtectedRoute component, add isAuthenticated useState and setter, add login and logout functions, wrap Home in ProtectedRoute so unauthorized users would be redirected to login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,31 @@
 import './App.css'
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './routes/Home'
 import Login from './routes/Login'
 import Signup from './routes/Signup'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  }
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  }
 
   return (
-    <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />} >
+        <Route path="home" element={<Home logout={logout} />} />
+      </Route>
+      
+      <Route path="/login" element={<Login login={login} />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   )
 }
 
