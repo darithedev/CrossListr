903e4e43f2891d7032b6e7e11e3b86bdbb153e30
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-29 00:58:26 -0400

    Add HomePorps type with logout function, add logout prop, add logout button that navigates to login p age on click

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,10 +1,18 @@
-const Home = () => {
+import { useNavigate } from 'react-router-dom'
+
+type HomeProps = {
+    logout: () => void
+};
+
+const Home = ({ logout }: HomeProps) => {
+    const navigate = useNavigate();
 
     return (
         <div className="home-container">
             <h1>Hello Home Route</h1>
+            <button onClick={() => {logout(), navigate('/login')}}>Logout</button>
         </div>
     )
-}
+};
 
 export default Home;
