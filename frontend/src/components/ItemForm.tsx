9c4fd3f57b95385ae90c431a6c850a1797305c5a
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-04-30 00:22:14 -0400

    Import useNavigate, axios, add const navigate for navigation path, add ItemData type, add item useState and setter

--- frontend/src/components/ItemForm.tsx
+++ frontend/src/components/ItemForm.tsx
@@ -1,7 +1,28 @@
 import { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const ItemForm = () => {
+    const navigate = useNavigate();
+
+    type ItemData = {
+        item_images: string[];
+        title: string;
+        description: string;
+        category: string;
+        condition: string;
+        price: number;
+    };
+
+    const [item, setItem] = useState<ItemData>({
+        item_images: [],
+        title: "",
+        description: "",
+        category: "",
+        condition: "",
+        price: 0
+    });
 
     return (
         <Form>
