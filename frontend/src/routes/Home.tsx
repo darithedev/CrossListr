8f6ca1a8783267443d9e7ace3d3bdbaed6b9961e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-01 21:11:39 -0400

    Add ItemCard component, add Item and Items type, add items useState and setter, add ItemCar component with items prop

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,9 +1,26 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
+import ItemCard from '../components/ItemCard';
+
+type Item = {
+    id: string;
+    item_images: string[];
+    title: string;
+    description: string;
+    category: string;
+    condition: string;
+    price: number;
+};
+
+type Items = Item[];
+
 const Home = () => {
     const navigate = useNavigate();
     const auth = useContext(UserContext);
+
+    // Items are pulled from GET /items endpoint
+    const [items, setItems] = useState<Items>([]);
 
     const handleLogout = () => {
         auth?.logout();
@@ -12,6 +29,7 @@ const Home = () => {
     return (
         <div className="home-container">
             <h1>Hello Home Route</h1>
+            <ItemCard items={items}/>
             <button onClick={() => handleLogout()}>Logout</button>
         </div>
     )
