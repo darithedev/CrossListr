0c18a8b99db92af6f15958cfb11c54a25ed2ee46
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-01 21:42:12 -0400

    Import ItemForm, add paths for items/new and items/:id/edit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,15 @@ import ProtectedRoute from './components/ProtectedRoute'
 import Home from './routes/Home'
 import Login from './routes/Login'
 import Signup from './routes/Signup'
+import ItemForm from './routes/ItemForm'
 
 function App() {
   return (
     <Routes>
       <Route element={<ProtectedRoute />} >
         <Route path="home" element={<Home />} />
+        <Route path="/items/new" element={<ItemForm />} />
+        <Route path="/items/:id/edit" element={<ItemForm />} />
       </Route>
       
       <Route path="/login" element={<Login />} />
