763db2fc1aafa1ca8350cdbd3df1ce3ae94e4eca
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-01 21:59:02 -0400

    Add PUT /items/:id endpoint

--- backend/src/routes/items.js
+++ backend/src/routes/items.js
@@ -107,6 +107,7 @@ router.get('/:id', authMiddleware, async (req, res) => {
             `SELECT 
                 items.id,
                 items.title,
+                items.description,
                 items.category, 
                 items.condition, 
                 items.price, 
@@ -130,6 +131,63 @@ router.get('/:id', authMiddleware, async (req, res) => {
         console.error('GET /items/:id failed:', error);
         return res.status(500).json({
             error: 'Error! Could not get item.'
+        });
+    }
+});
+
+router.put('/:id', authMiddleware, async (req,res) => {
+    try {
+        const { userId } = req.userId;
+        const { id } = req.params;
+        const { title, description, category, condition, price, item_images, source, externl_id} = req.body;
+
+        if (userId === null) {
+            return res.status(401).json({
+                error: 'Unauthenticated user.'
+            });
+        } else if (id === null) {
+            return res.status(400).json({
+                error: 'Could not find item.'
+            });
+        }
+
+        if (isNaN(userId) || isNaN(id)) {
+            return res.status(400).json({error: 'Invalid user id or item id.'});
+        };
+
+        if (!title || !description || !price) {
+            return res.status(400).json({error: 'Title, description, and price are required fields.'});
+        };
+
+        const result = await pool.query(
+            `UPDATE items 
+            SET title = $1, description = $2, category = $3, condition = $4, price = $5, item_images = $6, source = $7, external_id = $8
+            WHERE user_id = $9 AND id = $10
+            RETURNING 
+                items.id,
+                items.title,
+                items.description,
+                items.category, 
+                items.condition, 
+                items.price, 
+                items.item_images,
+                items.source, 
+                items.external_id,
+                items.created_at,
+                items.updated_at
+            `,
+            [title, description, category, condition, price, item_images, source, externl_id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Item not found.' });
+        };
+        
+        return res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.error('PUT /items/:id failed:', error);
+        return res.status(500).json({
+            error: 'Error! Could not update item.'
         });
     }
 });
