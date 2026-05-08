ce02c5fea9bfde57364d249b8304f971c464d4a2
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-08 01:38:16 -0400

    Add map to render all tags from tags prop

--- frontend/src/components/Tag.tsx
+++ frontend/src/components/Tag.tsx
@@ -6,7 +6,11 @@ const Tag = ({ tags }: TagProps) => {
 
     return (
         <div className="tag-container">
-
+            {tags.map((tag, index) => (
+                <span key={index} className="tag">
+                    {tag}
+                </span>
+            ))}
         </div>
     )
 }
