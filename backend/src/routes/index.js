dabbab47cafd9c3b8c2b8b201f9e5bdff9750927
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-11 22:30:22 -0400

    Import ebayRouter and mount at /ebay

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import authRouter from './auth.js'
 import itemsRouter from './items.js'
+import ebayRouter from './ebay.js'
 
 const router = express.Router();
 router.use('/auth', authRouter);
 router.use('/items', itemsRouter);
+router.use('/ebay', ebayRouter);
 
 export default router;
