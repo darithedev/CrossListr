33ddca5b222144311d9a8e7c34dc25f81a987883
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-13 23:18:13 -0400

    Refactor POST /items to return only id

--- backend/src/routes/items.js
+++ backend/src/routes/items.js
@@ -66,19 +66,7 @@ router.post('/', authMiddleware, async(req, res) => {
             `INSERT INTO items (user_id, title, description, category, condition, price, item_images, source, external_id)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
             RETURNING 
-                items.id,
-                items.user_id,
-                items.title,
-                items.description, 
-                items.category, 
-                items.condition, 
-                items.price, 
-                items.item_images,
-                items.source, 
-                items.external_id,
-                items.created_at,
-                items.updated_at
-            `,
+                items.id`,
             [userId, title, description, category, condition, price, item_images, source, external_id]
         );
         res.status(201).json(result.rows[0]);
