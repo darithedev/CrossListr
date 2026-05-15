f4a28f5783bc7977c70e87c1e6be195e1f2d2844
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-15 16:07:03 -0400

    Add ItemDetails route skeleton

---
+++ frontend/src/routes/ItemDetails.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from "react-router-dom";
+import axios from 'axios'
+
+const ItemDetails = () => {
+
+    return (
+        <>
+        </>
+    )
+}
+export default ItemDetails;
