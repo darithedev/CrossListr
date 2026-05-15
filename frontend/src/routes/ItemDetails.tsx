aa19a69aa5edb3ebedebdc303d82d8cd2821b0c7
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-15 16:12:11 -0400

    Add API_URL

--- frontend/src/routes/ItemDetails.tsx
+++ frontend/src/routes/ItemDetails.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from "react-router-dom";
 import axios from 'axios'
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 type Item = {
     id: string;
     item_images: string[];
