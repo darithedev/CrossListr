bbcf052da49deeaf988cdf806a3fc2798ae735c1
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-15 16:09:27 -0400

    Add Item type

--- frontend/src/routes/ItemDetails.tsx
+++ frontend/src/routes/ItemDetails.tsx
@@ -2,6 +2,16 @@ import { useState, useEffect } from 'react'
 import { useParams, useNavigate } from "react-router-dom";
 import axios from 'axios'
 
+type Item = {
+    id: string;
+    item_images: string[];
+    title: string;
+    description: string;
+    category: string;
+    condition: string;
+    price: number;
+};
+
 const ItemDetails = () => {
 
     return (
