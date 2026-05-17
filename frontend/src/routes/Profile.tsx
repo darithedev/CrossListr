b0f42d048f04579f573bc4a2bd821c4d5d6b0436
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-17 10:19:20 -0400

    Refactor return div, and use react bootstrap from instead, add profile form fields for name, email and phone Number (all disabled), add home and settings buttons

--- frontend/src/routes/Profile.tsx
+++ frontend/src/routes/Profile.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap'
 import { UserContext } from '../context/UserContext';
 
 const Profile = () => {
@@ -7,9 +8,44 @@ const Profile = () => {
     const auth = useContext(UserContext);
 
     return (
-        <div className="profile-container">
+        <Form className="profile-form">
             <h1>Profile</h1>
-        </div>
+
+            <Form.Group controlId="name">
+                <Form.Label>Name:</Form.Label>
+                <Form.Control
+                    type="text"
+                    required
+                    value={auth?.user?.name}
+                    disabled
+                >
+                </Form.Control>
+            </Form.Group>
+            
+            <Form.Group controlId="name">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                    type="text"
+                    required
+                    value={auth?.user?.email}
+                    disabled
+                >
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="name">
+                <Form.Label>Phone Number:</Form.Label>
+                <Form.Control
+                    type="tel"
+                    required
+                    value={auth?.user?.phone_number}
+                    disabled
+                >
+                </Form.Control>
+            </Form.Group>
+            <Button onClick={() => navigate('/home')}>Home</Button>
+            <Button onClick={() => navigate('/settings')}>Settings</Button>
+        </Form>
     )
 }
 
