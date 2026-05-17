a034366ecb362525aedc8e09b06f816d23ff54ae
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-17 09:52:56 -0400

    Import useContext, useNavogate, UserContext, add navigate const and auth useContext using UserContext

--- frontend/src/routes/Profile.tsx
+++ frontend/src/routes/Profile.tsx
@@ -1,4 +1,10 @@
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
+
 const Profile = () => {
+    const navigate = useNavigate();
+    const auth = useContext(UserContext);
 
     return (
         <div className="profile-container">
