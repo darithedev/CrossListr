6873b189e15cfd3c4eca0791bdbe35db8efb2024
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-17 10:28:12 -0400

    Add empty functions for fakebay and faketsy connections (emulating Oauth2), add integration div with integration header, fakebay and faketsy connection buttons

--- frontend/src/routes/Settings.tsx
+++ frontend/src/routes/Settings.tsx
@@ -1,8 +1,21 @@
 const Settings = () => {
 
+    const fakebayConnection = () => {
+
+    };
+
+    const faketsyConnection = () => {
+
+    };
+
     return (
         <div className="settings-container">
             <h1>Settings</h1>
+            <div className="integration">
+                <h3>Integration</h3>
+                <button type="button" onClick={fakebayConnection}>Connect to Fakebay</button>
+                <button type="button" onClick={faketsyConnection}>Connect to Faketsy</button>
+            </div>
         </div>
     )
 }
