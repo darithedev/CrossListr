7412934f7e991c14b4fb48cdf56c285813011124
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-05-19 21:55:06 -0400

    Fix but in external_imports table, marketplaces missing s

--- migrations/sql/00007_create_external_imports_table.sql
+++ migrations/sql/00007_create_external_imports_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE external_imports ( 
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    marketplace_id BIGINT NOT NULL REFERENCES marketplace(id) ON DELETE CASCADE,
+    marketplace_id BIGINT NOT NULL REFERENCES marketplaces(id) ON DELETE CASCADE,
     external_id VARCHAR(100),
     raw_data JSONB,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
