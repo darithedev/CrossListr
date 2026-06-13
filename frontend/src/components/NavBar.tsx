26179cb856c110fc2de3bc05b3cdbb8b3bce1893
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-06-12 20:09:58 -0400

    import useContext, useNavigate, and UserContext, add auth and navigate constants with auth validation

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,15 @@
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
+
 const NavBar = () => {
-    
+    const auth = useContext(UserContext);
+    const navigate = useNavigate();
+
+    if (!auth?.user) {
+        return null
+    }
+
     return (
         <div className="navbar-container">
             
